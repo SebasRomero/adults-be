69a15903c0e96633b8d398c254b82ffa0271e9a9
Sebastian <sebastian@coderlabs.co>
2024-10-20 07:55:29 -0400

    feat:: init project

---
+++ src/main/java/com/videos_be/adults/AdultsApplication.java
@@ -0,0 +1,13 @@
+package com.videos_be.adults;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdultsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdultsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/videos_be/adults/AdultsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videos_be.adults;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdultsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
