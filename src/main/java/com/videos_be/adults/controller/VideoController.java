ffd67e676cdd0d85634cbbf6ee04d3f0c8b29553
Sebastian <sebastian@coderlabs.co>
2024-10-20 20:18:44 -0400

    feat:: add videos controller

---
+++ src/main/java/com/videos_be/adults/controller/VideoController.java
@@ -0,0 +1,16 @@
+package com.videos_be.adults.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/video")
+public class VideoController {
+
+    @GetMapping()
+    String getAllVideos() {
+       return "Videos";
+    }
+}
