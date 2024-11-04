0d4d57b8af67a427f1b2fc4a7c637f419967db72
Luis Correa <luiscorrea874@gmail.com>
2024-11-04 17:58:28 -0500

    Get information by name

--- src/main/java/com/videos_be/adults/video/repository/VideoRepository.java
+++ src/main/java/com/videos_be/adults/video/repository/VideoRepository.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import com.videos_be.adults.video.model.VideoModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 public interface VideoRepository extends MongoRepository<VideoModel, String> {
 
-    Optional<VideoModel> existsByName(String name);
+    Page<VideoModel> findAllByNameLike(String name, Pageable pageable);
 
     //List<Tutorial> findByTitleContaining(String title);
     //List<Tutorial> findByPublished(boolean published);
