d1873f07628b37f13cc11a124cec2fa4a5bbc9b2
Sebastian <sebastian@coderlabs.co>
2024-11-04 19:14:51 -0500

    feat:: add timestamps to actresses

--- src/main/java/com/videos_be/adults/actress/model/ActressModel.java
+++ src/main/java/com/videos_be/adults/actress/model/ActressModel.java
@@ -4,9 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
@@ -14,6 +18,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EnableMongoAuditing()
 @Document(collection = "actress")
 public class ActressModel {
     @Id()
@@ -25,4 +30,20 @@ public class ActressModel {
     private String nationality;
     private List<String> categories;
     private Integer clickCount;
+    @CreatedDate
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+    public ActressModel(String id, String name, Date birth, Integer videosQuantity, String genre, String nationality, List<String> categories, Integer clickCount) {
+        this.id = id;
+        this.name = name;
+        this.birth = birth;
+        this.videosQuantity = videosQuantity;
+        this.genre = genre;
+        this.nationality = nationality;
+        this.categories = categories;
+        this.clickCount = clickCount;
+    }
 }
+
