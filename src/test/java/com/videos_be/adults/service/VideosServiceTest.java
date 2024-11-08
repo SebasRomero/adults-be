1770c16b6ccd96e7ff5a10e8008f54074fdf802c
Luis Correa <luiscorrea874@gmail.com>
2024-11-08 10:33:55 -0500

    feat:: dependencies and start testService

---
+++ src/test/java/com/videos_be/adults/service/VideosServiceTest.java
@@ -0,0 +1,35 @@
+package com.videos_be.adults.service;
+
+import com.videos_be.adults.video.repository.VideoRepository;
+import com.videos_be.adults.video.service.VideoService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+public class VideosServiceTest {
+
+    @Mock
+    private VideoRepository videoRepository;
+
+    @InjectMocks
+    private VideoService videoService;
+
+    @Test
+    void findAllVideos(){
+
+        //when(videoRepository.findAll()).thenReturn()
+
+
+
+
+
+
+
+    }
+
+
+}
